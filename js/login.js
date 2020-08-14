e0902d822d0e4eba6a77ce5ab7c1056ce09861ef
briangardiol <68928017+BrianGardiol@users.noreply.github.com>
2020-08-13 21:11:32 -0300

    Entrega01

---
+++ js/login.js
@@ -0,0 +1,12 @@
+var loggedin = sessionStorage.getItem("logged");
+
+ function check(form)
+{
+    {sessionStorage.setItem("logged","1"); location.href = "index.html";}
+}
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function(e){
+
+});
