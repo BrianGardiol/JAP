8c6e0eb2c5a97406a4902469e00cf09342e6ac9a
briangardiol <68928017+BrianGardiol@users.noreply.github.com>
2020-08-16 20:48:37 -0300

    Entega 01

--- js/login.js
+++ js/login.js
@@ -1,8 +1,12 @@
+
+
 var loggedin = sessionStorage.getItem("logged");
 
  function check(form)
 {
+    if (form.userid.value.length > 0 && form.password.value.length > 0)
     {sessionStorage.setItem("logged","1"); location.href = "index.html";}
+    else {cv()}
 }
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
@@ -10,3 +14,11 @@ var loggedin = sessionStorage.getItem("logged");
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+function cv() {
+    document.getElementById("campovacio").innerHTML= ""; 
+    var vacio = document.createTextNode("Debe introducir usario y contraseña");
+    document.getElementById("campovacio").appendChild(vacio);
+    document.getElementById("campovacio").style.marginTop = "10px";
+    document.getElementById("campovacio").style.fontFamily = "'Baloo Tamma 2', Regular";
+    document.getElementById("campovacio").style.color = "red"
+  }
